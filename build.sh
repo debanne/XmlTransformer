ad059cb8ab0f004f61df6bf05485001bcd1e6017
Guillaume Boucher <g.boucher@criteo.com>
2017-08-09 15:16:39 +0200

    build.sh - do not restore
    First restore fails on TestApp since the nuget
    package is not yet available in artifacts directory.

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 rm -rf artifacts
-dotnet restore TransformXmlTasks.sln
 dotnet pack src/TransformXmlTasks/TransformXmlTasks.csproj -c Release
 dotnet run --project test/TestApp/TestApp.csproj -c Debug --framework netcoreapp2.0
 dotnet run --project test/TestApp/TestApp.csproj -c Release --framework netcoreapp2.0
